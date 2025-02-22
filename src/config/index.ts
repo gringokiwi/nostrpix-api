cfab9f0bdbc160ff2c65de6a4fc93f3ed05b8884
gringokiwi <9779954+gringokiwi@users.noreply.github.com>
2025-02-22 02:08:23 -0300

    Rename debug to show_debug_data

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,4 @@ export const sqala_app_secret = process.env.SQALA_APP_SECRET;
 export const sqala_refresh_token = process.env.SQALA_REFRESH_TOKEN;
 export const supabase_url = process.env.SUPABASE_URL as string;
 export const supabase_key = process.env.SUPABASE_KEY as string;
-export const debug = process.env.DEBUG === "true" || false;
+export const show_debug_data = process.env.SHOW_DEBUG_DATA === "true" || false;
